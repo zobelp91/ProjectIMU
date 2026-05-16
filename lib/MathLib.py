9371f50f3423fb0f65e063ccc97ab0e394066966
Peter Zobel <peter-zobel@hotmail.de>
2026-05-16 14:35:29 +0200

    setting searchpaths etc

--- lib/MathLib.py
+++ lib/MathLib.py
@@ -1,7 +1,7 @@
 """ Mathematical Library
 """
 from math import sqrt
-from numpy import shape, matrix, size, long, NaN, append
+import numpy as np
 
 def pythagoras(*sites):
     """ calculates the pythagorean distance from a various number of arguments
@@ -12,47 +12,47 @@ def pythagoras(*sites):
     return sqrt(res)
 
 def toVector(a, b, c, d='none'):
-    """ transforms 3 or 4 scalars to a 3x1 numpy matrix
+    """ transforms 3 or 4 scalars to a 3x1 numpy np.matrix
     """
-    assert d == 'none' or isinstance(d, (int, long, float)), "Not a valid input for d"
-    if isinstance(d, (int, long, float)) :
-        vector = matrix([a, b, c, d])
+    assert d == 'none' or isinstance(d, (int, np.long, float)), "Not a valid input for d"
+    if isinstance(d, (int, np.long, float)) :
+        vector = np.matrix([a, b, c, d])
     else: 
-        vector = matrix([a, b, c])
+        vector = np.matrix([a, b, c])
     return vector.transpose()    
     
 
 def toValue(mat):
-    """ transforms a 3x1 numpy matrix to scalars
-        same as x1, x2, x3 = numpy.matrix()
+    """ transforms a 3x1 numpy np.matrix to scalars
+        same as x1, x2, x3 = numpy.np.matrix()
     """
-    dim = shape(mat)
+    dim = np.shape(mat)
     assert (max(dim) == 3 or max(dim) == 4) and min(dim) == 1, "Not a 3 or 4 dimensional vector" 
     if dim[0] > dim[1]: mat = mat.transpose()
     a = mat[0, 0]
     b = mat[0, 1]
     c = mat[0, 2]
-    if size(mat) == 4:
+    if np.size(mat) == 4:
         d = mat[0, 3]
         return a, b, c, d
     else:
         return a, b, c
 
 def resize(array1, array2):
-    """ adjust the dimension of two arrays by appending NaN
+    """ adjust the dimension of two arrays by np.appending np.nan
     """
-    if array1.shape > array2.shape:
-        array2 = append(array2, NaN)
-    elif array1.shape < array2.shape:
-        array1 = append(array1, NaN)
+    if array1.np.shape > array2.np.shape:
+        array2 = np.append(array2, np.nan)
+    elif array1.np.shape < array2.np.shape:
+        array1 = np.append(array1, np.nan)
     return array1, array2
 
 def mvMultiplication(vector1, vector2):
-    """ matrix-vector multiplication 
+    """ np.matrix-vector multiplication 
         returns a 4x1 vector
     """
     a, b, c, d = toValue(vector1)
-    return matrix([[a, -b, -c, -d], [b, a, -d, c], [c, d, a, -b], [d, -c, b, a]]) * vector2
+    return np.matrix([[a, -b, -c, -d], [b, a, -d, c], [c, d, a, -b], [d, -c, b, a]]) * vector2
             
 def runningAverage(old, new, weight):
     """ recursive running average
